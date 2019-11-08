fff5df6634f61485b7fdea0eb7c780d10c0dd461
emilandi <emilandi2000@hotmail.com>
2019-11-08 17:56:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
-searchUrbanDict = function(word){
-    var query = word.selectionText;
-    chrome.tabs.create({url: "https://listado.mercadolibre.com.ar/" + query});
- };
+var region='AR';  
 
+searchUrbanDict = function(word){        
+    var region = fnRegion();       
+    var query = word.selectionText;
+    chrome.tabs.create({url: "https://listado.mercadolibre.com." + region + "/" + query});
+};
+
+// evento menu contextual
 chrome.contextMenus.create({
- title: "Buscar '%s' en Mercado Libre",
- contexts:["selection"],  // ContextType
- onclick: searchUrbanDict // A callback function
+    title: "Buscar '%s' en Mercado Libre",
+    contexts:["selection"],  // ContextType
+    onclick: searchUrbanDict // A callback function
 });
+ 
+// retorna ubicacion cliente web
+function fnRegion () {    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {      
+        var obj = JSON.parse(xhttp.responseText);
+        region = obj.countryCode;        
+      }
+    };
+    xhttp.open("GET", "http://ip-api.com/json", true);
+    xhttp.send();
+    return region;
+}
+
+/*
+DOMINIOS
+AR
+BO
+BR
+CL
+CO
+CR
+DO
+EC
+BR
+HN
+MX
+NI
+PA
+PY
+PE
+SV
+UY
+VE
+*/
